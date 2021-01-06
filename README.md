c5670127157b51ad364b3fd59ad703c6fc1d4628
Tariq <t@redgoose.ca>
2021-01-06 15:40:08 -0500

    add integration test for /api/learning/foo endpoint

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * Creating a web API https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio
 * HttpClient https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=netcore-3.1
 * Swagger https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-5.0&tabs=netcore-cli
+* Integration testing https://docs.microsoft.com/en-us/dotnet/architecture/microservices/multi-container-microservice-net-applications/test-aspnet-core-services-web-apps#implementing-integration-and-functional-tests-for-each-microservice
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoApi;
 
 namespace Tests
 {
@@ -9,6 +14,25 @@ namespace Tests
         public void TestMethod1()
         {
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public async Task TestAPIFoo()
+        {
+            // Arrange
+            TestServer _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            HttpClient _client = _server.CreateClient();
+
+            // Act
+            var response = await _client.GetAsync("/api/learning/foo");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.AreEqual("foo", responseString);
+
         }
     }
 }
