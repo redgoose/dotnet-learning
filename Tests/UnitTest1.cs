5cf4a8c35ef0f2ed4bd3516156f72af077a37917
Tariq <t@redgoose.ca>
2021-01-14 11:12:48 -0500

    first attempt at using wiremock.net

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,12 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TodoApi;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private static WireMockServer _wireMockServer;
+
+        [ClassInitialize]
+        public static void StartMockServer(TestContext context)
+        {
+            _wireMockServer = WireMockServer.Start();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            _wireMockServer.Stop();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -32,7 +50,34 @@ namespace Tests
 
             // Assert
             Assert.AreEqual("foo", responseString);
-
         }
+
+        [TestMethod]
+        public async Task TestAPIBaz()
+        {
+            // Arrange
+            TestServer _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            HttpClient _client = _server.CreateClient();
+
+            _wireMockServer
+                //.Given(Request.Create().WithPath("https://api.ipify.org?format=json").UsingGet())
+                .Given(Request.Create().WithPath("?format=json").UsingGet())
+                .RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBody(@"{ ""msg"": ""Hello world!"" }")
+                );
+
+            // Act
+            var response = await _client.GetAsync("/api/learning/baz");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.AreEqual(@"{ ""msg"": ""Hello world!"" }", responseString);
+        }
+
     }
 }
