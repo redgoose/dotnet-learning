1c3d13acd31bb85df422f65a6c5df0a2f03dda19
Tariq <t@redgoose.ca>
2021-03-03 10:36:32 -0500

    remove WireMock.Net

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,30 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TodoApi;
-using WireMock.RequestBuilders;
-using WireMock.ResponseBuilders;
-using WireMock.Server;
-using WireMock.Settings;
-using TodoApi.Integrations;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        private static WireMockServer _wireMockServer;
-
-        [ClassInitialize]
-        public static void StartMockServer(TestContext context)
-        {
-            _wireMockServer = WireMockServer.Start();
-        }
-
-        [ClassCleanup]
-        public static void Cleanup()
-        {
-            _wireMockServer.Stop();
-        }
 
         [TestMethod]
         public void TestMethod1()
@@ -61,15 +43,6 @@ namespace Tests
                 .UseStartup<Startup>());
             HttpClient _client = _server.CreateClient();
 
-            _wireMockServer
-                //.Given(Request.Create().WithUrl("https://api.ipify.org?format=json").UsingGet())
-                .Given(Request.Create().WithPath("?format=json").UsingGet())
-                .RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithBody(@"{ ""ip"": ""0.0.0.0"" }")
-                );
-
             // Act
             var response = await _client.GetAsync("/api/learning/baz");
             response.EnsureSuccessStatusCode();
@@ -77,27 +50,7 @@ namespace Tests
             var responseString = await response.Content.ReadAsStringAsync();
 
             // Assert
-            Assert.AreEqual(@"{ ""ip"": ""0.0.0.0"" }", responseString);
-        }
-
-        [TestMethod]
-        public async Task TestAPIBaz2()
-        {
-            // Arrange
-            _wireMockServer
-                //.Given(Request.Create().WithUrl("https://api.ipify.org?format=json").UsingGet())
-                .Given(Request.Create().WithPath("?format=json").UsingGet())
-                .RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithBody(@"{ ""ip"": ""0.0.0.0"" }")
-                );
-
-            // Act
-            IPAddress ip = await IpifyClient.GetIPAddress();
-
-            // Assert
-            Assert.AreEqual("0.0.0.0", ip.ip);
+            Assert.IsTrue(responseString.Contains("ip"));
         }
 
     }
